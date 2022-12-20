cb076416c1054ef0b8c0edeb5b80a50cfd77bcb5
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:45:22 -0300

    Initial Commit

---
+++ Colors RGBa/Colors_RGBaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colors_RGBaApp.swift
+//  Colors RGBa
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Colors_RGBaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
